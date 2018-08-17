b533ab19b4bfcaa7f1aa0f737ce61bfa647e0ebd
Nenad Mikša <nenad.miksa@microblink.com>
2018-08-17 14:25:24 +0200

    added conanfile

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, CMake, tools
+
+
+class RapidJSONConan(ConanFile):
+    name = "RapidJSON"
+    version = "1.1.0"
+    license = "BSD"
+    url = "https://github.com/microblink/rapidjson"
+    description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
+    generators = "cmake"
+    scm = {
+        "type": "git",
+        "url": "auto",
+        "revision": "auto"
+    }
+    no_copy_source = True
+
+
+    def package(self):
+        self.copy("include/*.h", dst="")
+
+
+    def package_id(self):
+        self.info.header_only()
+
